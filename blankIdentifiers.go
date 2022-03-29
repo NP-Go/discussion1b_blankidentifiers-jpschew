3e23ad22534e08a54baf2ef8060c4d70cbb041c1
jpschew <jps.chew@gmail.com>
2022-03-29 21:25:19 +0800

    added comments to the code

--- blankIdentifiers.go
+++ blankIdentifiers.go
@@ -5,9 +5,11 @@ import "fmt"
 func main() {
 	stringA := "This is the message to print!"
 
-	//count, err := fmt.Println(stringA)
-	//fmt.Println(count, err)
+	// count - number of bytes printed
+	// err - error if there is any
+	count, err := fmt.Println(stringA)
+	fmt.Println(count, err)
 
-	_, err := fmt.Println(stringA)
-	fmt.Println(err)
+	// _, err := fmt.Println(stringA)
+	// fmt.Println(err)
 }
